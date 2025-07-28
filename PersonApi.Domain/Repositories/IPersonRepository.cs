6f1cf5a9308e22da7b0ac4863bcd45b11335f2ff
Tiago Catoia <tiagocsz@hotmail.com>
2025-07-28 16:24:06 -0300

    feat: add PersonRepository with implemented methods

--- PersonApi.Domain/Repositories/IPersonRepository.cs
+++ PersonApi.Domain/Repositories/IPersonRepository.cs
@@ -5,7 +5,7 @@ namespace PersonApi.Domain.Repositories;
 public interface IPersonRepository
 {
     Task<IEnumerable<Person>> GetAllAsync();
-    Task<Person> GetByIdAsync(int id);
+    Task<Person?> GetByIdAsync(int id);
     Task AddAsync(Person person);
     Task UpdateAsync(Person person);
     Task DeleteAsync(int id);
---
+++ PersonApi.Infrastructure/Repositories/PersonRepository.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PersonApi.Domain.Entities;
+using PersonApi.Domain.Repositories;
+using PersonApi.Infrastructure.Data;
+
+namespace PersonApi.Infrastructure.Repositories;
+
+public class PersonRepository(AppDbContext _context) : IPersonRepository
+{
+    public async Task<IEnumerable<Person>> GetAllAsync() => await _context.People.ToListAsync();
+
+    public async Task<Person?> GetByIdAsync(int id) => await _context.People.FindAsync(id);
+
+    public async Task AddAsync(Person person)
+    {
+        _context.People.Add(person);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task UpdateAsync(Person person)
+    {
+        _context.People.Update(person);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task DeleteAsync(int id)
+    {
+        var person = await _context.People.FindAsync(id);
+
+        if (person != null)
+        {
+            _context.People.Remove(person);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
